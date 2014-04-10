2af572aff5afead30f3bf79a2fa92d81cbca3722
slikts <dabas@untu.ms>
2014-04-11 01:11:02 +0300

    tests, readme

--- README.md
+++ README.md
@@ -1,3 +1,58 @@
 #isomorphine
 
-Transparently expose mongoose models on the client side through a secure RESTful API using an ACL.
+Makes it possible to use the same [mongoose](http://mongoosejs.com/) models both on the client and server side.
+The goal is to avoid having to define the data models more than once, or having to use different APIs to work with the database on the client and server side,
+or having to implement an API to work with the database.
+All that has to be provided to the client side are the model names and the REST API address, and the rest happens transparently.
+This is similar to what [Meteor](https://www.meteor.com/) does with mongodb.
+
+The REST API is implemeneted as an [express 4](http://expressjs.com/) router.
+
+### Example
+
+Defining the data model the server side:
+```
+var TestModel = mongoose.model('TestModel', new mongoose.Schema({
+  foo: Number,
+  bar: String
+}));
+
+TestModel.create({
+  foo: 42,
+  bar: 'baz'
+});
+```
+
+Running the REST API:
+
+```
+var Router = require('isomorphine/router');
+
+express()
+  .use(Router())
+  .listen();
+  .
+```
+
+Using the model on the client side:
+```
+// the server should somehow export Object.keys(mongoose.models) to the client
+var models = require('isomorphine/models')(['TestModel'], 'http://localhost:3000/');
+
+models.TestModel
+  .find({
+    foo: 42
+  })
+  .exec(function(err, data) {
+    if (err) {
+      return console.error(err);
+    }
+
+    console.log(data); // returns { foo: 42, bar: 'baz' }
+  });
+```
+
+### Planned features
+
+ * User authentication
+ * Model- and maybe document-level ACL
--- test/isomorphine.js
+++ test/isomorphine.js
@@ -5,25 +5,31 @@ var request = require('supertest');
 var express = require('express');
 
 var Router = require('../lib/router');
+var Model = require('../lib/client/model')();
+
+var _data = {
+  a: 1,
+  b: 2
+};
+
+var models = {
+  Test: {
+    find: function() {
+      return this;
+    },
+    exec: function(x) {
+      x(undefined, _data);
+      return this;
+    }
+  }
+};
 
 describe('Router', function() {
-  var models = {
-    'Test': {
-      a: function() {
-        return this;
-      },
-      b: function(x) {
-        x(undefined, 'qqq');
-        return this;
-      }
-    }
-  };
-
   var commands = [{
-    name: 'a',
+    name: 'find',
     args: []
   }, {
-    name: 'b',
+    name: 'exec',
     args: ['__fn']
   }];
 
@@ -46,12 +52,59 @@ describe('Router', function() {
         .post('/Test')
         .send(commands)
         .expect(200)
-        .expect('qqq')
+        .expect(JSON.stringify(_data))
         .end(function(err, res) {
           done(err);
         });
     });
-
   });
+});
 
+describe('client model', function() {
+  describe('app', function() {
+    var app = express();
+    var port = 3999;
+    var srv;
+
+    app
+      .use(function(req, res, next) {
+        res.writeHead(200, {
+          'Content-Type': 'application/json'
+        });
+        next();
+      })
+      .use(Router(models));
+
+    beforeEach(function() {
+      srv = app.listen(port);
+    });
+
+    // it('should call the api with commands', function(done) {
+
+    //   Model()
+    //     .find({
+    //       a: 1
+    //     })
+    //     .exec(function(err, data) {
+    //       done(err);
+    //     });
+
+    // });
+
+    it('should call the api with commands', function(done) {
+
+      Model(port)
+        .find()
+        .exec(function(err, data) {
+          if (!err) {
+            JSON.stringify(data).should.equal(JSON.stringify(_data));
+          }
+          done(err);
+        });
+    });
+
+    afterEach(function() {
+      srv.close();
+    });
+  });
 });
